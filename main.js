10c695560f83b4ff1581edb74f0c0d90c7cab8d3
Areeba Ahmed khan <159721838+gulariba@users.noreply.github.com>
2024-05-12 23:19:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+//switch statment
+let classTimings = "wednesday";
+switch (classTimings) {
+    case "sunday":
+        console.log("go to your class");
+        break;
+    case "monday":
+        console.log("go to your work");
+        break;
+    case "tuesday":
+        console.log("go to your work");
+        break;
+    case "wednesday":
+        console.log("take a sick leave");
+        break;
+    default:
+        console.log("holiday");
+}
